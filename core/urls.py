7c2e032e64d0bcee95b0138bd60f613c92b60e8e
N SH <bazoy@yandex.ru>
2023-05-19 21:49:47 +0300

    change_urls

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from core.views import RegistrationView, LoginView, ProfileView, UpdatePasswordView
 
 urlpatterns = [
-    path("sinnup/", RegistrationView.as_view(), name="sinnup"),
+    path("signup/", RegistrationView.as_view(), name="signup"),
     path("login/", LoginView.as_view(), name="login"),
     path("profile/", ProfileView.as_view(), name="profile"),
     path("update_password/", UpdatePasswordView.as_view(), name="update_password"),
