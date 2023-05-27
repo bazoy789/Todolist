c9e91d6737ea60c1a620c39089994c3d41aec231
N SH <bazoy@yandex.ru>
2023-05-27 12:42:03 +0300

    try5

--- goals/views/goal.py
+++ goals/views/goal.py
@@ -23,16 +23,13 @@ class GoalListView(generics.ListAPIView):
     search_fields = ["title", "description"]
 
     def get_queryset(self):
-        return Goal.objects.select_related("user").filter(
-            user=self.request.user, category__is_deleted=False
-        ).exclude(status=Goal.Status.archived)
+        return Goal.objects.select_related("user").filter(user=self.request.user, category__is_deleted=False).exclude(status=Goal.Status.archived)
 
 
 class GoalDetailView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [GoalPermission]
     serializer_class = GoalWithUserSerializer
-    queryset = Goal.objects.select_related("user").filter(
-        category__is_deleted=False).exclude(status=Goal.Status.archived)
+    queryset = Goal.objects.select_related("user").filter(category__is_deleted=False).exclude(status=Goal.Status.archived)
 
     def perform_destroy(self, instance: Goal) -> None:
         instance.status = Goal.Status.archived
